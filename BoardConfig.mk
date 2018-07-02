5caf75f56e5ce3470c760a6ded2804bf7bbf24f7
marc0601 <marckhalil06@gmail.com>
2018-07-02 21:16:33 +0200

    replace "reboot bootloader" with "reboot download"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,8 @@ BOARD_USES_MMCUTILS := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 TW_EXCLUDE_SUPERSU := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
 include $(LOCAL_PATH)/kernel.mk
 include device/generic/twrpbuilder/BoardConfig32.mk
 
