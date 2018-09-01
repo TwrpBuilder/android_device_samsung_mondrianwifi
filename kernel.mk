f4005086190d46ba069f50a133a160b8777410b6
marc0601 <36824857+marc0601@users.noreply.github.com>
2018-09-01 23:06:43 +0200

    test2

--- kernel.mk
+++ kernel.mk
@@ -19,5 +19,5 @@ TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00008000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt $(LOCAL_PATH)/dt.img
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_NEEDS_LZMA_MINIGZIP := true
